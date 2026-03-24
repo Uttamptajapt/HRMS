b673cd124b5abdd8fee1d3f2a90cff46eed02c36
Uttam Prajapat <uttam@evisionits.com>
2026-03-24 17:33:05 +0530

    Update OrganizationController.cs

--- HRMS.API/Controllers/OrganizationController.cs
+++ HRMS.API/Controllers/OrganizationController.cs
@@ -184,7 +184,7 @@ public class OrganizationController : ControllerBase
         });
     }
 
-    // DELETE Organization usinfg this API 
+    // DELETE Organization using this API 
     [HttpDelete("{id}")]
     [Authorize(Roles = "Admin")]
     [ProducesResponseType(typeof(ApiResponse<string>), StatusCodes.Status200OK)]
